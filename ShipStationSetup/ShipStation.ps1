c43a5cee661916b875a63845f3f2e53d7ffcf380
Jeffrey Wen <JeffreyW@sks-bottle.com>
2015-09-10 12:45:46 -0400

    Modify Ship Station Script for QOL changes
    * Fix indentation and capitalization
    * Use consistent statement separator (newline terminated)
    * Utilize Start-Process over Ampersand string execution
    
    Start-Process removes the sleep timer and pipes after each executable

--- ShipStationSetup/ShipStation.ps1
+++ ShipStationSetup/ShipStation.ps1
@@ -1,46 +1,35 @@
 ﻿<#
-SYNOPSYS
-This script will create a ship station when the script is run
-
-DESCRITPION
-The below script will copy GetScale the SKSNA01 drive, create the proper registry in the HKEY_LOCAL_MACHINE with the proper path, then will install
-the Connect ship program in the proper order
+SYNOPSIS
+Installs the necessary components for a functional Ship Station
 
 NOTES
 File name:  ShipStation.ps1
-Author:     Justin Neubauer
+Created by: Justin Neubauer
+Edited by:	Jeffrey Wen
 #>
 
+<# Directory exists #>
+if ( Test-Path "\\sksna01\Software\Standalone\Ship Station\" )
+{
+		<# Copy GetScale.exe to client computer #>
+		Copy-Item "\\sksna01\Software\Standalone\Ship Station\GetScale.exe" -Destination C:\
 
-<# This tests the path, If the path is correct then this is will move on to the installation part of the script. #>
-if (
-  test-path "\\sksna01\software\standalone\ship station\" )
+		<# Enable GetScale.exe to start on boot #>
+		New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name GetScale -PropertyType String -Value C:\GetScale.exe -Force
 
- {
+		<# Install ConnectShip Client, Core-C, and COM libraries #>
+		Start-Process "\\sksna01\Software\Standalone\Ship Station\ConnectShipToolkitClientSetup-6.5.exe"
+		Start-Process "\\sksna01\Software\Standalone\Ship Station\UpdateInstaller-6.5-Core-C.exe"
+		Start-Process "\\sksna01\Software\Standalone\Ship Station\UpdateInstaller-6.5-COM-Typelib-A.exe"
 
-<# This copies GetScale.exe from \\SKSNA01\Software\Standalone\Ship Station\GetScale.exe and places it in the C:\ directory #>
-Copy-Item '\\SKSNA01\Software\Standalone\Ship Station\GetScale.exe' -destination C:\ ;
-<# This creates a string in the registry where GetScale.exe executes on startup #>
-New-Itemproperty -path HKLM:\software\Microsoft\Windows\CurrentVersion\run -name "GetScale" -type "string" -value C:\GetScale.exe -force;
-<# This installs Connect Ship on the computer#>
-& '\\SKSNA01\Software\Standalone\Ship Station\ConnectShipToolkitClientSetup-6.5.exe' |
-<# This installs Core-C on the computer#>
-& '\\SKSNA01\Software\Standalone\Ship Station\UpdateInstaller-6.5-Core-C.exe' |
-<# This installs Library on the computer#>
-& '\\SKSNA01\Software\Standalone\Ship Station\UpdateInstaller-6.5-COM-Typelib-A.exe' |
+		<# Create popup to indicate completion #>
+		$wshell = New-Object -ComObject Wscript.Shell
+		$wshell.Popup("Restart your computer to complete the installation.", 0, "Setup Complete", 64)
+}
 
-<# Waits 0 Seconds after initializing the installer for the library before executing the next command #>
-Start-Sleep -s 0;
-<# The next two lines produces an output box #> 
-$wshell1 = New-Object -ComObject Wscript.Shell 
-$wshell1.Popup("To complete the installation of GetScale.exe, your computer must be restarted",0,"Setup Complete", 64)}
-
-
-
-<# this will run if the path is incorrect #>
+<# Directory DNE #>
 else {
-$wshell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
-$wshell.Popup("The file path is incorrect",0,"Done", 48)}
-
-
-
+	<# Create popup to indicate failure #>
+		$wshell = New-Object -ComObject Wscript.Shell -ErrorAction Stop
+		$wshell.Popup("Ship Station directory does not exist.", 0, "Done", 48)
+}
